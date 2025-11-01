c47296226fc64c1d63cd3a9e5e19b12302ca102f
correaemmanuel53-cmyk <correaemmanuel53@gmail.com>
2025-11-01 15:17:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import pandas as pd
-from influxdb_client import InfluxDBClient
 import plotly.express as px
+from influxdb_client import InfluxDBClient
+from dotenv import load_dotenv
+import os
+
+# Cargar variables del archivo .env
+load_dotenv()
+
+# Credenciales desde variables de entorno
+INFLUXDB_URL = os.getenv("INFLUXDB_URL")
+INFLUXDB_TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUXDB_ORG = os.getenv("INFLUXDB_ORG")
+INFLUXDB_BUCKET = os.getenv("INFLUXDB_BUCKET")
+
+# Configurar conexión con InfluxDB
+client = InfluxDBClient(
+    url=INFLUXDB_URL,
+    token=INFLUXDB_TOKEN,
+    org=INFLUXDB_ORG
+)
+query_api = client.query_api()
 
 # -----------------------------------
 # CONFIGURACIÓN GENERAL
